d95abbccfffc79e8225f2089556caff827beafd6
Vagner Gonçalves <88849441+vagnergon866@users.noreply.github.com>
2021-10-12 08:21:56 -0300

    Adicionando inrformações na classe TipoAtuacao.

--- desafio-resetFlix/api-resetFlix/src/main/java/br/com/cwi/reset/vagnergoncalves/TipoAtuacao.java
+++ desafio-resetFlix/api-resetFlix/src/main/java/br/com/cwi/reset/vagnergoncalves/TipoAtuacao.java
@@ -1,4 +1,17 @@
 package br.com.cwi.reset.vagnergoncalves;
 
 public enum TipoAtuacao {
+
+    PRINCIPAL("Principal"),
+    COADJUVANTE("Cosdjuvante");
+
+    private String descricao;
+    
+    TipoAtuacao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
