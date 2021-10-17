3998c37cd94861d33e219a630c7a1495bf8badfd
Vagner Gonçalves <88849441+vagnergon866@users.noreply.github.com>
2021-10-17 07:59:06 -0300

    Correção exercício 04.

--- Aula-03/Exercícios-Aula-03/primeiro-projeto-spring/src/main/java/br/com/reset/primeiroprojetospring/Controler/FilmeControler.java
+++ Aula-03/Exercícios-Aula-03/primeiro-projeto-spring/src/main/java/br/com/reset/primeiroprojetospring/Controler/FilmeControler.java
@@ -15,44 +15,48 @@ public class FilmeControler {
     private static List<Filme> filmes = new ArrayList<>();
 
     @PostMapping
-    public ResponseEntity<Filme> cadastrarFilme(@RequestBody Filme filme){
+    public ResponseEntity<Filme> cadastrarFilme(@RequestBody Filme filme) {
         Filme filmeExistente = buscarPorNome(filme.getNome());
-        if (filmeExistente != null){
+        if (filmeExistente != null) {
             return ResponseEntity.badRequest().build();
         }
         filmes.add(filme);
         return ResponseEntity.ok(filme);
     }
+
     @GetMapping
-    public List<Filme> consultarTodos(){
+    public List<Filme> consultarTodos() {
         return filmes;
     }
+
     @GetMapping("/{nome}")
-    public Filme buscarFilmePeloNome(@PathVariable String nome){
+    public Filme buscarFilmePeloNome(@PathVariable String nome) {
         return buscarPorNome(nome);
     }
 
 
     @DeleteMapping("/{nome}")
-    public void deleteFilme(@PathVariable String nome){
+    public void deleteFilme(@PathVariable String nome) {
         Filme filme = buscarFilmePeloNome(nome);
-        if(filme != null){
+        if (filme != null) {
             filmes.remove(filme);
         }
 
     }
+
     @PutMapping
-    public Filme atualizarFilme( @RequestBody Filme filme){
+    public Filme atualizarFilme(@RequestBody Filme filme) {
         Filme filmeCadastrado = buscarFilmePeloNome(filme.getNome());
-        if (filmeCadastrado != null){
+        if (filmeCadastrado != null) {
             filmes.remove(filmeCadastrado);
             filmes.add(filme);
         }
         return null;
     }
-    private Filme buscarPorNome(String nome){
-        for(Filme filme : filmes){
-            if(filme.getNome().equals(nome)){
+
+    private Filme buscarPorNome(String nome) {
+        for (Filme filme : filmes) {
+            if (filme.getNome().equals(nome)) {
                 return filme;
 
             }
