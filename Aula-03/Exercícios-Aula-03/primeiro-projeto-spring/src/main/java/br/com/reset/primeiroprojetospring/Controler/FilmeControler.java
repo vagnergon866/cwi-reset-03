78314bf0ef5310483fae01bfdaadb0a0b6cf7c31
Vagner Gonçalves <88849441+vagnergon866@users.noreply.github.com>
2021-10-17 07:56:37 -0300

    Correção Exercício 04.

--- Aula-03/Exercícios-Aula-03/primeiro-projeto-spring/src/main/java/br/com/reset/primeiroprojetospring/Controler/FilmeControler.java
+++ Aula-03/Exercícios-Aula-03/primeiro-projeto-spring/src/main/java/br/com/reset/primeiroprojetospring/Controler/FilmeControler.java
@@ -1,8 +1,10 @@
 package br.com.reset.primeiroprojetospring.Controler;
 
 import br.com.reset.primeiroprojetospring.domain.Filme;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.image.FilteredImageSource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,35 +14,49 @@ public class FilmeControler {
 
     private static List<Filme> filmes = new ArrayList<>();
 
-    @PostMapping("/{nome}")
-    public Filme cadastrarFilme(@RequestBody Filme filme){
-        filmes.add(filme);
-        return filme;
-
-    }
-
-    private String consultaFilmeNome(String nome) {
-        for (Filme filme : filmes) {
-            if (filme.getNome().equals(nome)) {
-                return nome;
-            }
+    @PostMapping
+    public ResponseEntity<Filme> cadastrarFilme(@RequestBody Filme filme){
+        Filme filmeExistente = buscarPorNome(filme.getNome());
+        if (filmeExistente != null){
+            return ResponseEntity.badRequest().build();
         }
-        return null;
+        filmes.add(filme);
+        return ResponseEntity.ok(filme);
     }
+    @GetMapping
+    public List<Filme> consultarTodos(){
+        return filmes;
+    }
+    @GetMapping("/{nome}")
+    public Filme buscarFilmePeloNome(@PathVariable String nome){
+        return buscarPorNome(nome);
+    }
+
+
     @DeleteMapping("/{nome}")
     public void deleteFilme(@PathVariable String nome){
-        String filme = consultaFilmeNome(nome);
+        Filme filme = buscarFilmePeloNome(nome);
         if(filme != null){
             filmes.remove(filme);
         }
 
     }
-    @PutMapping("/{nome}")
-    public void atualizarFilme(@PathVariable String nome, @RequestBody Filme filme){
-        String filmeCadastrado = consultaFilmeNome(filme.getNome());
+    @PutMapping
+    public Filme atualizarFilme( @RequestBody Filme filme){
+        Filme filmeCadastrado = buscarFilmePeloNome(filme.getNome());
         if (filmeCadastrado != null){
             filmes.remove(filmeCadastrado);
             filmes.add(filme);
         }
+        return null;
+    }
+    private Filme buscarPorNome(String nome){
+        for(Filme filme : filmes){
+            if(filme.getNome().equals(nome)){
+                return filme;
+
+            }
+        }
+        return null;
     }
 }
