0c91128b668f6efad0636d9c4636cecebe9a86cc
Vagner Gonçalves <88849441+vagnergon866@users.noreply.github.com>
2021-10-17 09:32:10 -0300

    Integrado o SpringBoot ao projeto.

--- desafio-resetFlix/api-resetFlix/src/main/java/br/com/cwi/reset/vagnergoncalves/Aplicacao.java
+++ desafio-resetFlix/api-resetFlix/src/main/java/br/com/cwi/reset/vagnergoncalves/Aplicacao.java
@@ -1,34 +1,14 @@
 package br.com.cwi.reset.vagnergoncalves;
 
-import domain.Ator;
-import domain.StatusCarreira;
-import request.AtorRequest;
-import service.AtorService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
+@SpringBootApplication
 public class Aplicacao {
 
+  
     public static void main(String[] args) throws Exception {
-        FakeDatabase fakeDatabase = new FakeDatabase();
-
-        AtorService atorService = new AtorService(fakeDatabase);
-
-        String nome = "Will Smith";
-        LocalDate dataNascimento = LocalDate.of(1968, Month.SEPTEMBER, 25);
-        StatusCarreira statusCarreira = StatusCarreira.EM_ATIVIDADE;
-        Integer anoInicioAtividade = 1986;
-        AtorRequest atorRequest = new AtorRequest(nome, dataNascimento, statusCarreira, anoInicioAtividade);
-
-       atorService.criarAtor(atorRequest);
-
-        List<Ator> atores = fakeDatabase.recuperaAtores();
-
-        System.out.println("Deve conter 1 ator, quantidade encontrada: " + atores.size());
-        System.out.println("Primeiro ator deve ser 'Will Smith', valor encontrado: " + atores.get(0).getNome());
-
+        SpringApplication.run(Aplicacao.class, args);
 
     }
 }
