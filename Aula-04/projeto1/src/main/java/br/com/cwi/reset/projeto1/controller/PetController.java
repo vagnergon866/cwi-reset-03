ed379ff5a916b4f8ccd2595296e06b7e3a914ba6
Vagner Gonçalves <88849441+vagnergon866@users.noreply.github.com>
2021-10-23 12:25:31 -0300

    Exercício 02

--- Aula-04/projeto1/src/main/java/br/com/cwi/reset/projeto1/controller/PetController.java
+++ Aula-04/projeto1/src/main/java/br/com/cwi/reset/projeto1/controller/PetController.java
@@ -4,6 +4,7 @@ import br.com.cwi.reset.projeto1.domain.Pet;
 import br.com.cwi.reset.projeto1.exception.PetJaExistenteException;
 import br.com.cwi.reset.projeto1.exception.PetNaoExistenteException;
 import br.com.cwi.reset.projeto1.service.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/pet")
 public class PetController {
-
+    
+    @Autowired
     private PetService service = new PetService();
 
     @GetMapping
